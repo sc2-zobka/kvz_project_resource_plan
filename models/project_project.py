82f52aaf7bc2440797d5f440af4e932a401416f5
sc2-zobka <math.bocaz@gmail.com>
2025-08-26 18:56:06 -0400

    [ADD] Total widget at the bottom of tec lines.

--- models/project_project.py
+++ models/project_project.py
@@ -10,3 +10,16 @@ class ProjectProject(models.Model):
         "project_id",
         string="Resource Lines",
     )
+    total_resource_amount = fields.Monetary(
+        string="Total Resource Amount",
+        compute="_compute_total_resource_amount",
+        store=True,
+        currency_field="currency_id",
+    )
+
+    @api.depends("resource_line_ids.subtotal_price")
+    def _compute_total_resource_amount(self):
+        for project in self:
+            project.total_resource_amount = sum(
+                project.resource_line_ids.mapped("subtotal_price")
+            )
